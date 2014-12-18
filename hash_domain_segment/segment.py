ca7cf8854c4588f0a6fe03a4801bfb5bfcc21702
domspad <domspad@umich.edu>
2014-12-18 13:52:18 -0800

    a scratch pad in ipynbs and the start of the program

---
+++ hash_domain_segment/segment.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+This program accepts a url or hashtag from stdin and prints the underlying phrase without the url and hashes :
+
+	ex: 
+			'www.ieatfishes.com'	 ----> 	'i eat fishes'
+			'#ILove3000' 			 ---->	'i love 3000'
+
+"""
+
+def clean( urlhash ) :
+	# returns urlhash without url decorations, hashtags, lowercase, and so just numbers and letters
+	return
+
+def get_next_token( urlhash ) :
+	#returns the longest possible token starting from the left
+	return
+
+if __name__ == '__main__' :
+	urlhash = raw_input()
+	print urlhash
+
