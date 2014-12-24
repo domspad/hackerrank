a66deb4da3d584700394a49d011df1f8b37b539c
domspad <domspad@umich.edu>
2014-12-24 11:30:31 -0500

    writing testing function

--- apple_disambiguation/which_apple.py
+++ apple_disambiguation/which_apple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 (Basic explanation)
 """
@@ -12,9 +11,28 @@ def whichApple(text) :
 	Requires: 'apple' to appear in text (with or without capitals)
 	Returns: 'fruit' or 'computer-company'
 	"""
+	words = text.split()
 
-	
+	prev = ''
+	for index,word in enumerate(words) :
+		#is word a variant of apple<'><s><'>?
+		if word[:5].lower() == 'apple' and len(word.lower().replace("'","")) <= 6 : #like applet...
+			#capital?
+			if word[0].isupper() :
+				+1 for company!
+			#plural?
+
+			#after end of sentence?
+
 	return 'computer-company' #or fruit
+
+def whichApple_tests() :
+	testcase_file = './test_cases.txt'
+	with open(testcase_file, 'r') as f :
+		num = int(f.readline())
+		for i in xrange(num) :
+			answer, text = f.readline().split('||')
+			assert whichApple(text) == answer
 
 if __name__ == '__main__' :
 
