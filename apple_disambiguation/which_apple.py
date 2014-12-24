e4db0040fb3be497fbccec21f8e0af06479c4eca
domspad <domspad@umich.edu>
2014-12-24 11:59:07 -0500

    naming convention corrections -- functions mixedCase, var lower_underscore

--- apple_disambiguation/which_apple.py
+++ apple_disambiguation/which_apple.py
@@ -11,26 +11,38 @@ def whichApple(text) :
 	Requires: 'apple' to appear in text (with or without capitals)
 	Returns: 'fruit' or 'computer-company'
 	"""
-	words = text.split()
+	instance, location = extractApple(text)
 
-	prev = ''
-	for index,word in enumerate(words) :
-		#is word a variant of apple<'><s><'>?
-		if word[:5].lower() == 'apple' and len(word.lower().replace("'","")) <= 6 : #like applet...
-			#capital?
-			if word[0].isupper() :
-				+1 for company!
-			#plural?
+	#coding the decision tree model
+	if instance[0].isupper() :
+		if sentenceStart(text, index) :
+			if isPlural(instance) :
+				return 'fruit'
+			else :
+				return 'computer-company'
+		else :
+			return 'computer-company'
+	else :
+		return 'fruit'
 
-			#after end of sentence?
-
-	return 'computer-company' #or fruit
-
-def extract_apple(text) :
+def extractApple(text) :
 	"""
 	Requires: some form of 'apple' shows up in text (i.e. capitalized, plural, possessive)
 	Returns: the form of the first instance found and its starting index in the text
 	"""
+	# words = text.split()
+
+	# prev = ''
+	# for index,word in enumerate(words) :
+	# 	#is word a variant of apple<'><s><'>?
+	# 	if word[:5].lower() == 'apple' and len(word.lower().replace("'","")) <= 6 : #like applet...
+	# 		#capital?
+	# 		if word[0].isupper() :
+	# 			+1 for company!
+	# 		#plural?
+
+	# 		#after end of sentence?
+	return 'Apple',0
 
 def whichApple_tests() :
 	testcase_file = './test_cases.txt'
@@ -42,6 +54,8 @@ def whichApple_tests() :
 	return
 
 if __name__ == '__main__' :
+
+	whichApple_tests()
 
 	#formatted to accept Hackerrank like input
 	iterations = int(raw_input())
